c00ccd288b446c841cc9f11b33bcc1fddf742e68
arielle23 <38578580+arielle23@users.noreply.github.com>
2018-04-23 20:32:19 +0800

    add

---
+++ let_lanmbda.scm
@@ -0,0 +1,12 @@
+#lang sicp
+(define (f x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
+
+(define (square a)
+  (* a a))
+
+(f 2 3)
