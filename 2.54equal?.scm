e3415b885cb218228967b6db166cabfb55d94b42
邓巧 <ariellechn97@gmail.com>
2018-09-06 00:25:11 +0800

    18-9-5

---
+++ 2.54equal?.scm
@@ -0,0 +1,10 @@
+(define (equal? lst1 lst2)
+    (cond ((and (null? lst1) (null? lst2)) true)
+          ((and (pair? (car lst1)) (pair? (car lst2)))
+            (and (equal? (car lst1) (car lst2)) (equal? (cdr lst1) (cdr lst2))))
+          ((eq? (car lst1) (car lst2))
+            (equal? (cdr lst1) (cdr lst2)))
+          (else
+            false)))
+(equal? `(this is a list) `(this (is a) list))
+(car `a)
